ce4cfcee6b144969dd8e3c186ef7a406059d58f7
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2018-11-26 21:09:27 +0300

    First init

---
+++ Telemetry.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Telemetry.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

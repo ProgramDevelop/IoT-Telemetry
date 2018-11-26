dfbb77d0815f8b80a5c30d43852b28848f05edaf
Dasha <dasha@desktop-s3j2llt>
2018-11-26 22:30:42 +0300

    Created User,Sensor,SensorValue models

---
+++ Telemetry.Database/Models/Sensor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Telemetry.Database.Models
+{
+    public class Sensor
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Sensor> Values { get; set; }
+    }
+}
---
+++ Telemetry.Database/Models/SensorValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Telemetry.Database.Models
+{
+    public enum ValueType
+    {
+        String = 0,
+        Number = 1,
+    }
+    
+    public class SensorValue
+    {
+        public Guid Id { get; set; }
+        public Guid SensorId { get; set; }
+        public Sensor Sensor { get; set; }
+        public string Name { get; set; }
+        public ValueType Type { get; set; }
+        public string Value { get; set; }
+    }
+}

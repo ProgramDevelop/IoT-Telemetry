50efd612dfdce45b4b5eccfbd412061124a4f449
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2018-12-16 12:50:49 +0300

    Added link sensor to user

--- Telemetry.Database/Models/Sensor.cs
+++ Telemetry.Database/Models/Sensor.cs
@@ -6,6 +6,8 @@ namespace Telemetry.Database.Models
     public class Sensor
     {
         public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Sensor> Values { get; set; }

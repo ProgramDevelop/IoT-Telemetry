a414c8c93ae50a1f5a170668b5f3901a228d8dfe
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2018-12-23 13:10:17 +0300

    Update .Net Core to 2.2

--- Telemetry.Web/Startup.cs
+++ Telemetry.Web/Startup.cs
@@ -42,7 +42,7 @@ namespace Telemetry.Web
                   options.LoginPath = new PathString("/User/Login");
               });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

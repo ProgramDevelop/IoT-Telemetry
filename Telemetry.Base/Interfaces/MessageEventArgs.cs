10853250b61c9f83af72e353d42c83b8034837e4
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2018-12-25 22:13:32 +0300

    Again modified receiver interface

---
+++ Telemetry.Base/Interfaces/MessageEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Telemetry.Base.Interfaces
+{
+    public class MessageEventArgs
+    {
+        public Guid SensorId { get; set; }
+        public string ValueName { get; set; }
+        public ISensorData Payload { get; set; }
+    }
+}

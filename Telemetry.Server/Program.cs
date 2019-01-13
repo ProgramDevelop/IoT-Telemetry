87cd9621828aa5baa12e34bdf116eded9fffb39f
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2019-01-14 01:31:28 +0300

    Create Server manager to sends results and load plugins

--- Telemetry.Server/Program.cs
+++ Telemetry.Server/Program.cs
@@ -9,27 +9,13 @@ namespace Telemetry.Server
     {
         static void Main(string[] args)
         {
-            //TODO: Create load function
             //TODO: Create background task
             var directory = AppDomain.CurrentDomain.BaseDirectory;
 
-            var files = Directory.GetFiles(directory, "*.dll");
+            var serverManager = new ServerManager("http://localhost:5000", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoicGF2ZWwua3MxOTk2QGdtYWlsLmNvbSIsInN1YiI6InBhdmVsLmtzMTk5NkBnbWFpbC5jb20iLCJuYmYiOjE1NDc0MTgxNDAsImV4cCI6MTU1MDAxMDE0MCwiaXNzIjoiU01UVS1EZXYiLCJhdWQiOiJTTVRVLURldi1TZW5zb3JTZXJ2ZXIifQ.UdPpvSUG1vEEKsbHCRPZH2H59w_wUKIIadyMnGQFIRs");
+            serverManager.Load(directory);
 
-            foreach (var file in files)
-            {
-                var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file);
-
-                var assemblyTypes = assembly.GetTypes();
-
-                foreach (var type in assemblyTypes)
-                {
-                    var iface = type.GetInterface(nameof(IReceiver));
-                    if (iface != null)
-                    {
-                        var plugin = (IReceiver)Activator.CreateInstance(type);
-                    }
-                }
-            }
+            serverManager.Start();
         }
     }
 }
---
+++ Telemetry.Server/ServerManager.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Loader;
+using System.Text;
+using Telemetry.Base.Interfaces;
+
+namespace Telemetry.Server
+{
+    class ServerManager
+    {
+        private readonly List<IReceiver> _receivers;
+        private string _uri;
+        private readonly string _apiKey;
+
+        public ServerManager(string apiUri, string apiKey)
+        {
+            _uri = apiUri;
+            _apiKey = apiKey;
+            _receivers = new List<IReceiver>();
+        }
+
+        public void Load(string directory)
+        {
+            var files = Directory.GetFiles(directory, "*.dll");
+
+            foreach (var file in files)
+            {
+                var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file);
+
+                var assemblyTypes = assembly.GetTypes();
+
+                foreach (var type in assemblyTypes)
+                {
+                    var receiverType = type.GetInterface(nameof(IReceiver));
+                    if (receiverType != null)
+                    {
+                        var plugin = (IReceiver)Activator.CreateInstance(type);
+                        _receivers.Add(plugin);
+                    }
+                }
+            }
+        }
+
+        public void Start()
+        {
+            foreach (var receiver in _receivers)
+            {
+                receiver.Start();
+                receiver.OnMessageReceive += Receiver_OnMessageReceive;
+            }
+        }
+
+        private void Receiver_OnMessageReceive(MessageEventArgs obj)
+        {
+        }
+
+        private async void SendData(Guid sensorId, string name, ISensorData payload)
+        {
+            var bodyJson = new JObject();
+            bodyJson.Add("Name", name);
+            var payloadJson = JToken.FromObject(payload);
+            bodyJson.Add("Payload", payloadJson);
+
+            string body = bodyJson.ToString();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_uri);
+                var request = new HttpRequestMessage(HttpMethod.Post, $"/api/sensors/{sensorId}/value/set")
+                {
+                    Content = new StringContent(body, Encoding.UTF8, "application/json")
+                };
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _apiKey);
+
+                var response = await client.SendAsync(request);
+                var responseStr = await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
